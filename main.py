8df998356757b9c9f4b675f1c9a4fca1f92b51ce
Saveliy19 <saveliy200319@gmail.com>
2024-05-14 19:39:52 +0300

    Added logging

--- main.py
+++ main.py
@@ -2,5 +2,44 @@ from fastapi import FastAPI
 
 from app.routes import router
 
+import logging
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import Response
+import time
+
+# Настройка логирования в файл
+logging.basicConfig(
+    level=logging.ERROR,  # Уровень логирования ERROR
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='app.log',  # Имя файла для логов
+)
+
 app = FastAPI()
+
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        start_time = time.time()
+        inf = f"Запрос: {request.method} {request.url} Headers: {request.headers} Body: {await request.body()}"
+        response = await call_next(request)
+        
+        # Если статус ответа - ошибка (код >= 400), записываем в лог
+        if response.status_code >= 400:
+            # Логгирование ошибки
+            execution_time = time.time() - start_time
+            logging.error(f"Ошибка: {response.status_code} {request.method} {request.url} Время выполнения: {execution_time:.2f} сек, {inf}")
+        
+        return response
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+app.add_middleware(LoggingMiddleware)
+
 app.include_router(router)
